659992f23a23a67f22c01d8d543e5e2d6a4425dd
RicardoReyes44 <rickyboy1002@gmail.com>
2021-02-08 12:40:40 -0600

    Se agrego la clase 'Interfaz'

--- src/Prueba.java
+++ src/Prueba.java
@@ -1,11 +1,22 @@
 import javax.swing.*;
 
+class Interfaz{
+	
+}
 
 
 public class Prueba {
 
 	public static void main(String[] args) {
 		
+        SwingUtilities.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				new Interfaz();
+			}
+		});
 
 
 

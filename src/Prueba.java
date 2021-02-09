421fb44cd821a4001a30d48ec4164bf7fa6072e9
RicardoReyes44 <rickyboy1002@gmail.com>
2021-02-08 23:46:28 -0600

    Se agrego la etiqueta 'lbl7' en la clase 'Interfaz'

--- src/Prueba.java
+++ src/Prueba.java
@@ -17,45 +17,56 @@ class Interfaz extends JFrame{
 		setVisible(true);
 
 		JLabel lbl1 = new JLabel("The Classic Form includes all visible fields for");
-		establecerPosicion(0, 0, 1, 1, lbl1, 0, null);
+		establecerPosicion(0, 0, 1, 1, lbl1, null);
 
 		JLabel lbl2 = new JLabel("this list");
-		establecerPosicion(0, 1, 1, 1, lbl2, 1, null);
+		establecerPosicion(0, 1, 1, 1, lbl2, null);
 
 		JLabel lbl3 = new JLabel("FROM OPTIONS");
-		establecerPosicion(0, 2, 2, 1, lbl3, 0,  new Dimension(100, 40));
+		establecerPosicion(0, 2, 1, 1, lbl3, new Dimension(100, 40));
 
 		JLabel lbl4 = new JLabel("Include following");
-		establecerPosicion(0, 4, 1, 1, lbl4, 0, null);
+		establecerPosicion(0, 3, 1, 1, lbl4, null);
 		
 		JCheckBox chk = new JCheckBox("a little for your form");
-		establecerPosicion(0, 5, 1, 1, chk, 0, null);
+		establecerPosicion(0, 4, 1, 1, chk, null);
 		
-		JTextField txt1 = new JTextField("Suscribe to our mailing list");
-		establecerPosicion(0, 6, 1, 1, txt1, 0, null);
+		JTextField txt1 = new JTextField("Suscribe to our mailing list", 23);
+		establecerPosicion(0, 5, 1, 1, txt1, null);
+		
+		JLabel lbl5 = new JLabel("");
+		establecerPosicion(0, 6, 1, 1, lbl5, new Dimension(50, 50));
 		
 		JRadioButton rb = new JRadioButton("only required fields");
-		establecerPosicion(0, 7, 1, 1, rb, 0, new Dimension(80, 80));
+		establecerPosicion(0, 7, 1, 1, rb, null);
 		
 		JRadioButton rb2 = new JRadioButton("all fields");
-		establecerPosicion(0, 8, 1, 1, rb2, 0, null);
+		establecerPosicion(0, 8, 1, 1, rb2, null);
+		
+		JLabel lbl6 = new JLabel("(edit required fields in the form builder)");
+		establecerPosicion(0, 9, 1, 1, lbl6, null);
+		
+		JLabel lbl7 = new JLabel("");
+		establecerPosicion(0, 10, 1, 1, lbl7, new Dimension(50, 50));
 		
 		pack();
 		setLocationRelativeTo(null);
 
 	}
 	
-	public void establecerPosicion(int x, int y, int height, int width, Component componente, int relleno, Dimension padding) {
+	public void establecerPosicion(int x, int y, int height, int width, Component componente, Dimension padding) {
 		gbc.gridx = x;
 		gbc.gridy = y;
 		gbc.gridheight = height;
 		gbc.gridwidth = width;
-		
+
 		if(padding!=null){
 			componente.setPreferredSize(padding);
+			gbc.anchor = GridBagConstraints.SOUTHWEST;
+		}else {
+			gbc.anchor = GridBagConstraints.WEST;
 		}
-		
-		gbc.anchor = GridBagConstraints.WEST;
+
 		gbl.setConstraints(componente, gbc);
 		add(componente);
 	}

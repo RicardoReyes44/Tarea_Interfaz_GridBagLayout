8af9ab0286ee2ebb55e5b9bdbe69360c9f39310c
RicardoReyes44 <rickyboy1002@gmail.com>
2021-02-08 18:07:25 -0600

    Se agrego la clgregaron las etiquetas 'lbl3' y 'lbl4'

--- src/Prueba.java
+++ src/Prueba.java
@@ -1,4 +1,7 @@
 import java.awt.*;
+import java.awt.font.TextAttribute;
+import java.util.Map;
+
 import javax.swing.*;
 
 class Interfaz extends JFrame{
@@ -14,17 +17,26 @@ class Interfaz extends JFrame{
 		setVisible(true);
 
 		JLabel lbl1 = new JLabel("The Classic Form includes all visible fields");
-		establecerPosicion(0, 0, 1, 1, lbl1, 0);
+		establecerPosicion(0, 0, 1, 1, lbl1, 0, null);
 
 		JLabel lbl2 = new JLabel("this list");
-		establecerPosicion(0, 1, 1, 1, lbl2, 1);
+		establecerPosicion(0, 1, 1, 1, lbl2, 1, null);
 
+		JLabel lbl3 = new JLabel("FROM OPTIONS");
+		int a[] = {40, 40};
+		establecerPosicion(0, 3, 1, 1, lbl3, 0, a);
+
+		JLabel lbl4 = new JLabel("Include following");
+		establecerPosicion(0, 5, 1, 1, lbl4, 0, null);
+		
+		
+		
 		pack();
 		setLocationRelativeTo(null);
 
 	}
 	
-	public void establecerPosicion(int x, int y, int height, int width, Component componente, int relleno) {
+	public void establecerPosicion(int x, int y, int height, int width, Component componente, int relleno, int[] padding) {
 		gbc.gridx = x;
 		gbc.gridy = y;
 		gbc.gridheight = height;
@@ -32,6 +44,10 @@ class Interfaz extends JFrame{
 		
 		if(relleno!=0){
 			gbc.fill = GridBagConstraints.HORIZONTAL;
+		}
+		
+		if(padding!=null){
+			componente.setPreferredSize(new Dimension(padding[0], padding[1]));
 		}
 		
 		gbl.setConstraints(componente, gbc);
